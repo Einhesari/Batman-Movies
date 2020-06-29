2775be1f97b53b574a1d89d5a0ff319eeae29089
Mohsen Einhesari <mohseneinhesari@yahoo.com>
2020-06-29 19:40:53 +0430

    add movies usecase module

---
+++ app/src/main/java/com/einhesari/batmanmovies/di/module/UseCaseModule.kt
@@ -0,0 +1,14 @@
+package com.einhesari.batmanmovies.di.module
+
+import com.einhesari.batmanmovies.data.repository.MovieRepositoryImpl
+import com.einhesari.batmanmovies.domain.usecase.MoviesUseCase
+import dagger.Module
+import dagger.Provides
+
+@Module
+class MoviesUsecaseModule {
+    @Provides
+    fun provideMoviesUseCase(repository: MovieRepositoryImpl): MoviesUseCase {
+        return MoviesUseCase(repository)
+    }
+}
