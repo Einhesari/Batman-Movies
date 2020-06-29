4e662d14235094332fe4f5a0563e496edf812321
Einhesari <mohseneinhesari@yahoo.com>
2020-06-29 11:21:22 +0430

    add dagger app component and init it in app class

--- app/src/main/java/com/einhesari/batmanmovies/App.kt
+++ app/src/main/java/com/einhesari/batmanmovies/App.kt
@@ -1,11 +1,22 @@
 package com.einhesari.batmanmovies
 
 import android.app.Application
+import com.einhesari.batmanmovies.di.component.AppComponent
+import com.einhesari.batmanmovies.di.component.DaggerAppComponent
+
+lateinit var component: AppComponent
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initDagger()
+    }
+
+    private fun initDagger() {
+        component = DaggerAppComponent
+            .factory()
+            .create(this)
     }
 
 }
